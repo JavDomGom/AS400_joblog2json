514722be39519db4153f9f1b381e27195132a45f
JavierDominguezGomez <jdomgo3@gmail.com>
2020-12-05 21:17:41 +0100

    Process first line from job user's log.

--- main.c
+++ main.c
@@ -5,48 +5,69 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define MAXSTRLEN 10
+#define MAXWORD 9
+
 bool prefix();
 
 int main(int argc, char * argv[])
 {
 	if (argc < 2) return 1;
+
 	char * filename = argv[1];
 	char * iniPrefix = "INICIO";
 	char * endPrefix = "FIN";
 	char * line = NULL;
-	FILE * fp;
+	char * IBMiOSProgram_Name;
+	char * IBMiOSProgram_Version;
+	int IBMiOSProgram_Size;
+	int n_line = 0;
 	size_t len = 0;
 	ssize_t read;
+	FILE * fp;
 
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		exit(EXIT_FAILURE);
 
-	while ((read = getline(&line, &len, fp)) != -1) {
-		printf("%s", line);
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		printf("[%d] %s", n_line, line);
 
-		if (prefix(line, iniPrefix, strlen(iniPrefix))) {
-			char * user;
-			char const *needle = "USUARIO";
-			size_t needle_length = strlen(needle);
-			char const *needle_pos = strstr(line, needle);
+		if (prefix(line, iniPrefix, strlen(iniPrefix)))
+		{
+			char arr[MAXWORD][MAXSTRLEN + 1] = {0};
+			char *pch;
+			int i = 0;
+			pch = strtok (line," ");
 
-			// not found, at end of str or not preceded and followed by whitespace:
-			if (!needle_pos || !needle_pos[needle_length] || !isspace((char unsigned)needle_pos[needle_length]) ||
-				needle_pos != line && !isspace((char unsigned)needle_pos[-1]))
+			while (pch != NULL && i < MAXWORD)
 			{
-				fprintf(stderr, "\"%s\" couldn't be found. :(\n\n", needle);
-				return EXIT_FAILURE;
-			}   
+				strncpy(arr[i++], pch, MAXSTRLEN);
+				pch = strtok (NULL, " ");
+			}
+			
+			char * user = arr[4];
+			char * jobName = arr[5];
+			char * date = arr[6];
+			char * time = arr[7];
+			char * timeZone = arr[8];
+			size_t dateLen = strlen(date);
+			size_t timeLen = strlen(time);
+			size_t timeZoneLen = strlen(timeZone);
 
-			// extract the user following the word at needle_pos:
-			sscanf(needle_pos + needle_length, "%99s", user);
-			printf("Found \"%s\" after \"%s\"\n\n", user, needle);
+			// 3 additional bytes for spaces and EOL.
+			char logDateTime[dateLen + timeLen + timeZoneLen + 3];
+			
+			snprintf(logDateTime, sizeof logDateTime, "%s %s %s", date, time, timeZone);
 		}
 
 		if (prefix(line, endPrefix, strlen(endPrefix))) {
 			printf("Press ENTER key to continue ...\n");
 			getchar();
+			n_line = 0;
+		} else {
+			n_line++;
 		}
 	}
 
